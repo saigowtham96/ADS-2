cf4311cd58ed017ce0d406c6873c0fa2876e4ba6
saigowtham96 <sai.gauthi.gowtham1@msitprogram.net>
2018-11-05 01:23:08 +0530

    "ads2_m6_a1_1: 5/12 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- m6/ADS-2Week-1/Solution.java
+++ m6/ADS-2Week-1/Solution.java
@@ -1,156 +1,150 @@
 import java.util.Scanner;
-import java.util.HashMap;
-import java.util.ArrayList;
 /**
- *the graph is send to page rank class.
- *first of all I am building a hashMap which is having.
- *all the vertices inlinks.
- *form that iam going to find out the rank of each vertex
- *for every iteration.
- *initially the pagerank value will be 1/ vertices.
- *it is iteration is done for 1000 times.
- */
-/**
- *page rank class is for calculating.
- *page rank of each web page.
+ * class to find the pagerank.
  */
 class PageRank {
     /**
-     *the vertices of graph.
+     * graph as g.
+     */
+    private Digraph gr;
+    /**
+     * reverse of the given graph as revG.
+     */
+    private Digraph revdg;
+    /**
+     * variable for vertices.
      */
     private int vertices;
     /**
-     *the map object to store the.
-     *each web page vertex and page rank.
+     * array to store the pageRanks.
      */
-    private HashMap<Integer, Double> map;
-    // HashMap<Integer, ArrayList<Integer>> inLinks;
+    private Double[] pr;
     /**
-    *graph object.
-    */
-    private DiGraph graph;
-    /**
-     *the reverse graph object.
-     */
-    private DiGraph revGraph;
-    /**
-     *constructor to initialize.
+     * constructor.
      *
-     * @param      g graph object
+     * @param      gph    The graphics
      */
-    PageRank(final DiGraph g) {
-        graph = g;
-        vertices = graph.vertices();
-        map = new HashMap<Integer, Double>();
-        // inLinks = new HashMap<Integer, ArrayList<Integer>>();
-        revGraph = graph.reverse();
-    }
-    /**
-     *calculate page rank.
-     *of each web page.
-     *time complexity is O()
-     */
-    public void calculatePR() {
-        Double sum = 0.0;
-        int count = 0;
-        ArrayList<Integer> list;
-        double temp = (double) vertices;
-        double initialPR = (1 / temp);
+    PageRank(final Digraph gr) {
+        this.gr = gr;
+        this.revdg = gr.reverse();
+        this.vertices = gr.V();
+        pr = new Double[vertices];
+        int ver = gr.V();
         for (int i = 0; i < vertices; i++) {
-            if (graph.indegree[i] == 0) {
-                map.put(i, 0.0);
-            } else {
-                map.put(i, initialPR);
-            }
+            pr[i] = 1.0 / ver;
         }
-        // for (int i = 0; i < vertices; i++) {
-        //  for (int w : graph.adj[i]) {
-        //      list = new ArrayList<Integer>();
-        //      if (inLinks.containsKey(w)) {
-        //          ArrayList<Integer> tempList = inLinks.get(w);
-        //          tempList.add(i);
-        //          inLinks.put(w, tempList);
-        //      } else {
-        //          list.add(i);
-        //          inLinks.put(w, list);
-        //      }
-        //  }
-        // }
-        final int thousand = 1000;
-        double[] tempArray = new double[graph.vertices()];
-        for (int j = 1; j < thousand; j++) {
-            for (int i = 0; i < vertices; i++) {
-                sum = 0.0;
-                for (int each : revGraph.adj(i)) {
-                    double value = map.get(each);
-                    sum += ((double) value / (double) graph.outdegree(each));
-                }
-                tempArray[i] = sum;
-            }
-            for (int i = 0; i < vertices; i++) {
-                map.put(i, tempArray[i]);
-            }
-        }
+        prCalculation();
     }
-    /**
-     *this method is to print the
-     *vertex and its page rank.
+    /**.
+     * method to calculate the page Rank
      */
-    public void print() {
-        for (int i = 0; i < map.size(); i++) {
-            System.out.println(i + " - " + map.get(i));
-        }
-    }
-}
-/**
- *class for main method.
- */
-public final class Solution {
-    /**
-     *an empty constructor.
-     */
-    private Solution() {
-
-    }
-    /**
-     *main method is  for user input.
-     *
-     * @param      args  The arguments
-     */
-    public static void main(final String[] args) {
-        // read the first line of the input to get the number of vertices
-        Scanner scan = new Scanner(System.in);
-        DiGraph graph = new DiGraph(scan);
-        System.out.println(graph);
-        for (int i = 0; i < graph.vertices(); i++) {
-            if (graph.outdegree(i) == 0) {
-                for (int k = 0; k < graph.vertices(); k++) {
-                    if (k != i) {
-                        graph.addEdge(i, k);
+    public void prCalculation() {
+        for (int i = 0; i < vertices; i++) {
+            if (gr.outdegree(i) == 0) {
+                for (int j = 0; j < vertices; j++) {
+                    if (i != j) {
+                        gr.addEdge(i, j);
                     }
                 }
             }
         }
+        final int thousand = 1000;
+        for (int k = 1; k < thousand; k++) {
+            Double[] temppr = new Double[vertices];
+            for (int i = 0; i < vertices; i++) {
+                Double newpr = 0.0;
+                for (int ele : gr.reverse().adj(i)) {
+                    newpr = newpr
+                    + pr[ele] / gr.outdegree(ele);
+                }
+                temppr[i] = newpr;
+            }
+            pr = temppr;
+        }
+    }
+    /**.
+     * method to get the page rank for the given page rank
+     *
+     * @param      v     { vertices of type int }
+     *
+     * @return     The page rank.
+     */
+    public Double getPageRank(final int v) {
+        return pr[v];
+    }
+    /**.
+     * method to printer
+     */
+    public void display() {
+        for (int i = 0; i < vertices; i++) {
+            System.out.println(i + " - " + pr[i]);
+        }
+    }
+}
+/**.
+ * class to for web search
+ */
+class WebSearch {
+//Websearch ckass
+}
+
+/**.
+ * solution class
+ */
+final class Solution {
+    /**.
+     * constructor
+     */
+    private Solution() {
+        //Constructor
+    }
+    /**.
+     * main method to handle the input testcases
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        /**.
+         * Scanner object
+         */
+        Scanner s = new Scanner(System.in);
+        // read the first line of the
+        // input to get the number of vertices
+
+        int no_v = Integer.parseInt(s.nextLine());
+        Digraph dg = new Digraph(no_v);
         // iterate count of vertices times
         // to read the adjacency list from std input
         // and build the graph
-        // Create page rank object and pass the graph object to the constructor
-        PageRank prObj = new PageRank(graph);
-        prObj.calculatePR();
-        prObj.print();
+        for (int i = 0; i < no_v; i++) {
+            String[] tokens = s.nextLine().split(" ");
+            for (int j = 1; j < tokens.length; j++) {
+                int ve = Integer.parseInt(tokens[0]);
+                int adj = Integer.parseInt(tokens[j]);
+                dg.addEdge(ve, adj);
+            }
+        }
+        System.out.println(dg.toString());
+        PageRank obj = new PageRank(dg);
+        // Create page rank object to
+        //pass the graph object to the constructor
+        obj.display();
+
         // print the page rank object
 
         // This part is only for the final test case
 
         // File path to the web content
-        // String file = "WebContent.txt";
+        String file = "WebContent.txt";
 
         // instantiate web search object
-        // and pass the page rank object and the file path to the constructor
+        // and pass the page rank object
+        // and the file path to the constructor
 
         // read the search queries from std in
         // remove the q= prefix and extract the search word
         // pass the word to iAmFeelingLucky method of web search
         // print the return value of iAmFeelingLucky
+
     }
 }
