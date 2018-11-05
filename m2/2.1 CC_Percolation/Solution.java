f91b518a02490c08045b7a2fb4984533a75aeb02
saigowtham96 <sai.gauthi.gowtham1@msitprogram.net>
2018-11-05 21:25:35 +0530

    "ads2_m2_a1_1: 24/24 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 1/1 "

--- m2/2.1 CC_Percolation/Solution.java
+++ m2/2.1 CC_Percolation/Solution.java
@@ -1,116 +1,171 @@
 import java.util.Scanner;
 /**
- * Class for solution.
+ * Class for percolation.
  */
-class Perculation {
-    /**
-     * number of vertices.
-     */
-    private int vertices;
-    /**
-     * this method checks input is perculated or not
-     * and returns the boolean value.
-     * Time complexity is O(N^2).
-     * addEdge method of graph method is called untill all
-     * edges are connected.
-     *
-     * @param      grid  The grid
-     * @param      graphObj    The graphics
-     * @param      vertices1    The vertices
-     *
-     * @return     returns boolean value.
-     */
-    public boolean perculation(final boolean[][] grid,
-                               final Graph graphObj,
-                               final int vertices1) {
-        vertices = vertices1;
-        for (int i = 0; i < vertices; i++) {
-            for (int j = 0; j < vertices; j++) {
-                if (grid[i][j]) {
-                    int tmp = simplify(i, j);
-                    if (i == 0) {
-
-                        graphObj.addEdge(
-                            tmp, vertices * vertices);
-                    }
-                    if (i == vertices - 1) {
-                        graphObj.addEdge(tmp,
-                                         vertices * vertices + 1);
-                    }
-                    if (i - 1 >= 0
-                        && grid[i - 1][j]) {
-                        graphObj.addEdge(tmp,
-                                         simplify(i - 1, j));
-                    }
-                    if (i + 1 < vertices
-                        && grid[i + 1][j]) {
-                        graphObj.addEdge(
-                            tmp, simplify(i + 1, j));
-                    }
-                    if (j - 1 >= 0
-                        && grid[i][j - 1]) {
-                        graphObj.addEdge(tmp,
-                                         simplify(i, j - 1));
-                    }
-                    if (j + 1 < vertices
-                        && grid[i][j + 1]) {
-                        graphObj.addEdge(
-                            tmp, simplify(i, j + 1));
-                    }
-
-                }
-            }
+class Percolation {
+  /**
+   * Constructs the object.
+   */
+  Percolation() {
+    // Blank constructor.
+  }
+  /**
+   * its a boolean array.
+   */
+  private boolean[] array;
+  /**
+   * int size variable.
+   */
+  private int size;
+  /**
+   * graph.
+   */
+  private Graph graph;
+  /**
+   *array size.
+   */
+  private int arrSize;
+  /**
+   * Intializing count.
+   */
+  private int count;
+  /**
+   * first row.
+   */
+  private int top;
+  /**
+   * last row.
+   */
+  private int bottom;
+  /**
+   * Constructs the object.
+   *
+   * @param      num     { parameter_description }
+   */
+  Percolation(final int num) {
+        this.arrSize = num;
+        this.size = num * num;
+        this.top = size;
+        this.bottom = size + 1;
+        this.count = 0;
+        graph = new Graph(size + 2);
+        array = new boolean[size];
+        for (int i = 0; i < arrSize; i++) {
+            graph.addEdge(top, i);
+            graph.addEdge(bottom, size - i - 1);
         }
-        DepthFirstPaths dp = new DepthFirstPaths(
-            graphObj, vertices * vertices);
-        return dp.hasPathTo(vertices * vertices + 1);
+    }
+  /**
+     * This method converts two dimensional to one dimensional.
+     *
+     * @param      row   The row
+     * @param      col   The col
+     *
+     * @return  oneDimensional array
+     */
+    public int toOneDimension(final int row, final int col) {
+        return (arrSize * (row - 1)) + (col - 1);
     }
     /**
-     * this method gives the edge of graph.
-     * Time complexity is O(1).
+     * This method Connects the open sites.
      *
-     * @param      row     row
-     * @param      column     column
-     *
-     * @return     returns the edge of the graph.
+     * @param      row   The row
+     * @param      col   The col
      */
-    public int simplify(final int row, final int column) {
-        return row * vertices + column;
+    private void connectOpenSites(final int row, final int col) {
+        if (array[col] && !graph.hasEdge(row, col)) {
+            graph.addEdge(row, col);
+        }
+    }
+    /**
+     * This method opens the blocked site.
+     *
+     * @param      row     The row
+     * @param      col  The column
+     */
+    public void open(final int row, final int col) {
+        int index = toOneDimension(row, col);
+        array[index] = true;
+        count++;
+        int toprow = index - arrSize;
+        int bottomrow = index + arrSize;
+        if (arrSize == 1) {
+            graph.addEdge(top, index);
+            graph.addEdge(bottom, index);
+        }
+        if (bottomrow < size) {         //bottom
+            connectOpenSites(index, bottomrow);
+        }
+        if (toprow >= 0) {              //top
+            connectOpenSites(index, toprow);
+        }
+        if (col == 1) {                 //left
+            if (col != arrSize) {
+                connectOpenSites(index, index + 1);
+            }
+            return;
+        }
+        if (col == arrSize) {         //right
+            connectOpenSites(index, index - 1);
+            return;
+        }
+        connectOpenSites(index, index + 1);
+        connectOpenSites(index, index - 1);
+    }
+    /**
+     * Checks if open.
+     *
+     * @param      row   The row
+     * @param      col   The col
+     *
+     * @return     True if open, False otherwise.
+     */
+    public boolean isOpen(final int row, final int col) {
+        return array[toOneDimension(row, col)];
+    }
+    /**
+     * return number of open sites.
+     *
+     * @return count
+     */
+    public int numberOfOpenSites() {
+        return count;
+    }
+    /**
+     * method to check whether there is a flow.
+     *
+     * @return boolean
+     */
+    public boolean percolates() {
+        CC connectedComponents = new CC(graph);
+        return connectedComponents.connected(top, bottom);
     }
 }
 /**
  * Class for solution.
  */
-final class Solution {
+public final class Solution {
     /**
      * Constructs the object.
      */
-    private Solution() {
-
+    protected Solution() {
+      // Blank constructor.
     }
     /**
-     * this main method performs operations
-     * Time complexity is O(N^2).
+     * this main method performs operations.
      *
-     * @param      args  The arguments
+     * @param args String
      */
     public static void main(final String[] args) {
-        Scanner scan = new Scanner(System.in);
-        int vertices = Integer.parseInt(scan.nextLine());
-        Perculation obj = new Perculation();
-        boolean[][] grid = new boolean[vertices][vertices];
-        Graph gObj = new Graph(vertices * vertices + 2);
-        try {
-            while (scan.hasNext()) {
-                int row = scan.nextInt(), col = scan.nextInt();
-                grid[row - 1][col - 1] = true;
-
-            }
-        } catch (Exception e) {
-
-        } finally {
-            System.out.println(obj.perculation(
-                                   grid, gObj, vertices));
+        Scanner sc = new Scanner(System.in);
+        int vertices = Integer.parseInt(sc.nextLine());
+        Percolation obj = new Percolation(vertices);
+        while (sc.hasNext()) {
+            String[] tokens = sc.nextLine().split(" ");
+            obj.open(Integer.parseInt(tokens[0]),
+                      Integer.parseInt(tokens[1]));
         }
+        System.out.println(obj.percolates()
+                           && obj.numberOfOpenSites() != 0);
     }
 }
