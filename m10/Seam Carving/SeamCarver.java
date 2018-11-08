52f95bfda8832fdd839c9d89953f381d9771c58e
saigowtham96 <sai.gauthi.gowtham1@msitprogram.net>
2018-11-08 18:05:12 +0530

    "ads2_m10_a1_1: 5/9 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- m10/Seam Carving/SeamCarver.java
+++ m10/Seam Carving/SeamCarver.java
@@ -1,13 +1,18 @@
 import java.awt.Color;
-/**
+/**.
  * Class for seam carver.
  */
 public class SeamCarver {
-    /**
+    /**.
      * { variable picture}
      */
     private Picture picture;
     // create a seam carver object based on the given picture
+    /**.
+     * Constructs the object.
+     *
+     * @param      picture1  The picture 1
+     */
     public SeamCarver(final Picture picture1) {
         if (picture1 == null) {
             throw new IllegalArgumentException("picture is null");
@@ -15,22 +20,42 @@ public class SeamCarver {
             this.picture = picture1;
         }
     }
-    // current picture 
+    // current picture
+    /**.
+     * {returns the picture}
+     *
+     * @return     { description_of_the_return_value }
+     */
     public Picture picture() {
         return this.picture;
     }
     // width of current picture
+    /**.
+     * {returns the width}
+     *
+     * @return     { description_of_the_return_value }
+     */
     public int width() {
         return picture.width();
     }
-
     // height of current picture
+    /**
+     * { returns the height }
+     *
+     * @return     { description_of_the_return_value }
+     */
     public int height() {
         return picture.height();
     }
-
     // energy of pixel at column x and row y
-
+    /**
+     * { returns the energy }
+     *
+     * @param      x     { parameter_description }
+     * @param      y     { parameter_description }
+     *
+     * @return     { description_of_the_return_value }
+     */
     public double energy(int x, int y) {
         if (x == 0 || y == 0 || picture.width() - 1 == x || picture.height() - 1 == y) {
             return 1000;
