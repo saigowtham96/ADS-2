1947024d5e8eb7beb59d292477b3f75541b5b3c6
saigowtham96 <sai.gauthi.gowtham1@msitprogram.net>
2018-11-09 12:31:36 +0530

    "ads2_m10_a1_1: 5/9 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- m10/Seam Carving/SeamCarver.java
+++ m10/Seam Carving/SeamCarver.java
@@ -53,15 +53,15 @@ public class SeamCarver {
      *
      * @param      x     { parameter_description }
      * @param      y     { parameter_description }
-     *
+      *
      * @return     { description_of_the_return_value }
      */
-    public double energy(final int x,final int y) {
+    public double energy(final int x, final int y) {
         if (x == 0 || y == 0 || picture.width() - 1 == x || picture.height() - 1 == y) {
             return 1000;
         } else {
             Color top = picture.get(x, y + 1);
-            Color bottom = picture.get(x, y - 1); 
+            Color bottom = picture.get(x, y - 1);
             Color right = picture.get(x - 1, y);
             Color left = picture.get(x + 1, y);
             int rh = Math.abs(right.getRed() - left.getRed());
@@ -74,7 +74,7 @@ public class SeamCarver {
             int vertical = rv * rv + bv * bv + gv * gv;
             double energy = Math.sqrt(horizontal + vertical);
             return energy;
-        }       
+        }     
     }
     // sequence of indices for horizontal seam
     public int[] findHorizontalSeam() {
@@ -87,12 +87,12 @@ public class SeamCarver {
     }
 
     // remove horizontal seam from current picture
-    public void removeHorizontalSeam(int[] seam) {
+    public void removeHorizontalSeam(final int[] seam) {
 
     }
 
     // remove vertical seam from current picture
-    public void removeVerticalSeam(int[] seam) {
+    public void removeVerticalSeam(final int[] seam) {
 
     }
 }
