baa2e8cf2afe6fa8935a10462649bccdd362bd1d
saigowtham96 <sai.gauthi.gowtham1@msitprogram.net>
2018-11-10 12:48:50 +0530

    "ads2_m12_a1_1: 5/6 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- m12/ADS-2_Exam-2/Solution.java
+++ m12/ADS-2_Exam-2/Solution.java
@@ -3,53 +3,54 @@ import java.util.Scanner;
  * Class for solution.
  */
 public class Solution {
-	/**.
-	 * main method Time complexity  is O(E)
+    /**.
+     * main method Time complexity  is O(E)
      * E is number of edges.
      * V for printing graph.
      * E+V for dijkstra's constructor.
      * V for pathTo method.
-	 * @param      args  The arguments
-	 */
-	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
-		int vertices = Integer.parseInt(scan.nextLine());
-		int edges = Integer.parseInt(scan.nextLine());
-		EdgeWeightedGraph graph = new EdgeWeightedGraph(vertices);
-		for(int i = 0;i < edges;i++){
-			String[] inputs = scan.nextLine().split(" ");
-			graph.addEdge(new Edge(Integer.parseInt(inputs[0]), Integer.parseInt(inputs[1]),
-				Double.parseDouble(inputs[2])));
-		}
-		String caseToGo = scan.nextLine();
-		switch (caseToGo) {
-		case "Graph":
-			System.out.println(graph);
-			break;
-		case "DirectedPaths":
-			// Handle the case of DirectedPaths, where two integers are given.
-			// First is the source and second is the destination.
-			// If the path exists print the distance between them.
-			// Other wise print "No Path Found."
-			String[] paths = scan.nextLine().split(" ");
-			DijkstraSP object = new DijkstraSP(graph, Integer.parseInt(paths[0]));
-			if(object.hasPathTo(Integer.parseInt(paths[1]))){
-				System.out.println(object.distTo(Integer.parseInt(paths[1])));
-			} else{
-				System.out.println("No Path Found.");
-			}
-			break;
-		case "ViaPaths":
-			// Handle the case of ViaPaths, where three integers are given.
-			// First is the source and second is the via is the one where path should pass throuh.
-			// third is the destination.
-			// If the path exists print the distance between them.
-			// Other wise print "No Path Found."
-			String[] viaPaths = scan.nextLine().split(" ");
-			DijkstraSP object1 = new DijkstraSP(graph, Integer.parseInt(viaPaths[0]));
-			DijkstraSP object2 = new DijkstraSP(graph, Integer.parseInt(viaPaths[1]));
-			if(object1.hasPathTo(Integer.parseInt(viaPaths[1]))&&object2.hasPathTo(Integer.parseInt(viaPaths[2]))) {
-				System.out.println(object1.distTo(
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int vertices = Integer.parseInt(scan.nextLine());
+        int edges = Integer.parseInt(scan.nextLine());
+        EdgeWeightedGraph graph = new EdgeWeightedGraph(vertices);
+        for (int i = 0; i < edges; i++) {
+            String[] inputs = scan.nextLine().split(" ");
+            graph.addEdge(new Edge(
+                            Integer.parseInt(inputs[0]), Integer.parseInt(inputs[1]),
+                Double.parseDouble(inputs[2])));
+        }
+        String caseToGo = scan.nextLine();
+        switch (caseToGo) {
+        case "Graph":
+            System.out.println(graph);
+            break;
+        case "DirectedPaths":
+            // Handle the case of DirectedPaths, where two integers are given.
+            // First is the source and second is the destination.
+            // If the path exists print the distance between them.
+            // Other wise print "No Path Found."
+            String[] paths = scan.nextLine().split(" ");
+            DijkstraSP object = new DijkstraSP(graph, Integer.parseInt(paths[0]));
+            if (object.hasPathTo(Integer.parseInt(paths[1]))){
+                System.out.println(object.distTo(Integer.parseInt(paths[1])));
+            } else {
+                System.out.println("No Path Found.");
+            }
+            break;
+        case "ViaPaths":
+            // Handle the case of ViaPaths, where three integers are given.
+            // First is the source and second is the via is the one where path should pass throuh.
+            // third is the destination.
+            // If the path exists print the distance between them.
+            // Other wise print "No Path Found."
+            String[] viaPaths = scan.nextLine().split(" ");
+            DijkstraSP object1 = new DijkstraSP(graph, Integer.parseInt(viaPaths[0]));
+            DijkstraSP object2 = new DijkstraSP(graph, Integer.parseInt(viaPaths[1]));
+            if(object1.hasPathTo(Integer.parseInt(viaPaths[1]))&&object2.hasPathTo(Integer.parseInt(viaPaths[2]))) {
+                System.out.println(object1.distTo(
                                        Integer.parseInt(
                                            viaPaths[1])) + object2.distTo(
                                        Integer.parseInt(viaPaths[2])));
@@ -60,9 +61,13 @@ public class Solution {
                                            viaPaths[2])));
            
 
-			} else {
-				System.out.println("No Path Found.");
-			}
-	}
+            } else {
+                System.out.println("No Path Found.");
+            }
+            break;
+            default:
+            break;
+
+    }
 }
 }
