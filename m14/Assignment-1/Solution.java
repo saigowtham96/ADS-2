f3992e1cb9b424c793d3f46c38df987df376e1a9
saigowtham96 <sai.gauthi.gowtham1@msitprogram.net>
2018-11-19 17:48:29 +0530

    "ads2_m14_a1_1: 6/6 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 1/1 "

--- m14/Assignment-1/Solution.java
+++ m14/Assignment-1/Solution.java
@@ -2,36 +2,42 @@ import java.util.Scanner;
 /**.
  * Class for solution.
  */
-public class Solution {
-	public static void main(String[] args) {
-		String[] words = loadWords();
-		//Your code goes here...
-		TST<Integer> tst = new TST<Integer>();
-		Scanner scan = new Scanner(System.in);
-		String prefix = scan.nextLine();
-		int j =0;
-		for (String each : words) {
-			SuffixArray suffix = new SuffixArray(each);
-			for (int i = 0;i<each.length();i++) {
+final class Solution {
+    /**
+     * Constructs the object.
+     */
+    private Solution() {
 
-			tst.put(suffix.select(i),j++);
+    }
+    public static void main(final String[] args) {
+        String[] words = loadWords();
+        //Your code goes here...
+        TST<Integer> tst = new TST<Integer>();
+        Scanner scan = new Scanner(System.in);
+        String prefix = scan.nextLine();
+        int j =0;
+        for (String each : words) {
+            SuffixArray suffix = new SuffixArray(each);
+            for (int i = 0;i<each.length();i++) {
 
-		}
-	}
-		 for (String each : tst.keysWithPrefix(prefix)) {
+            tst.put(suffix.select(i),j++);
+
+        }
+    }
+         for (String each : tst.keysWithPrefix(prefix)) {
             System.out.println(each);
         }
     
 
-	}
+    }
     /**.
      * Loads words.
      *
      * @return     { description_of_the_return_value }
      */
-	public static String[] loadWords() {
-		In in = new In("/Files/dictionary-algs4.txt");
-		String[] words = in.readAllStrings();
-		return words;
-	}
+    public static String[] loadWords() {
+        In in = new In("/Files/dictionary-algs4.txt");
+        String[] words = in.readAllStrings();
+        return words;
+    }
 }
